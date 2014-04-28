548a836a1f1a3b275ceccc79b3448d9cdd7100d2
plantalog <github2@aquaplant.ee>
2014-04-28 18:37:15 +0200

    40byte status packets

--- cadiweb/cm/cadi_response_parser.php
+++ cadiweb/cm/cadi_response_parser.php
@@ -25,7 +25,7 @@ switch ($packet_type) {
 	break;
 	case 3:
 		// STATUS data provider
-		$block_id = ord($last_packet[20]);
+		$block_id = ord($last_packet[36]);
 		echo 'something--separator--';
 		switch ($block_id) {
 			case 1:
@@ -52,10 +52,10 @@ switch ($packet_type) {
 				$cadi_time = $cadi_ta[3]*16777216+$cadi_ta[2]*65536+$cadi_ta[1]*256+$cadi_ta[0];
 
 
-				$crc = ord($last_packet[21]);
-				$counted_crc = crc_block(2, $last_packet, 21);	// 90 xor 88 = 2
+				$crc = ord($last_packet[($packet_size-3)]);
+				$counted_crc = crc_block(2, $last_packet, ($packet_size-3));	// 90 xor 88 = 2
 				if ($counted_crc==$crc) {
-					
+					// echo 'CRC OK';
 					$_SESSION['cadi_status']['dht']['temp'] = ($dht[2]*256+$dht[3])/10;	// DHT temperature
 					$_SESSION['cadi_status']['dht']['rh'] = ($dht[0]*256+$dht[1])/10;	// DHT sensor relative humidity
 					$_SESSION['cadi_status']['timerStateFlags'] = decbin(ord($last_packet[3]));
@@ -98,8 +98,8 @@ switch ($packet_type) {
 				$adc_avg[1] = ord($last_packet[8])+ord($last_packet[9])*256;
 				$adc_avg[2] = ord($last_packet[10])+ord($last_packet[11])*256;
 
-				$crc = ord($last_packet[13]);
-				$counted_crc = crc_block(2, $last_packet, 13);	// 90 xor 88 = 2, 14 - the rest of 16byte "ZX3..." packet
+				$crc = ord($last_packet[($packet_sizde-3)]);
+				$counted_crc = crc_block(2, $last_packet, ($packet_sizde-3));	// 90 xor 88 = 2, 14 - the rest of 16byte "ZX3..." packet
 				if ($counted_crc==$crc) {
 					$_SESSION['cadi_status']['time'] = $cadi_time;	// Cadi UNIXTIME
 					$_SESSION['cadi_status']['adc_avg'][0] = $adc_avg[0];	// ADC average value
@@ -137,8 +137,8 @@ switch ($packet_type) {
 				// valve_failed flags
 				$valve_failed = decbin(ord($last_packet[10]));
 
-				$crc = ord($last_packet[13]);
-				$counted_crc = crc_block(2, $last_packet, 13);	// 90 xor 88 = 2, 14 - the rest of 16byte "ZX3..." packet
+				$crc = ord($last_packet[($packet_sizde-3)]);
+				$counted_crc = crc_block(2, $last_packet, ($packet_sizde-3));	// 90 xor 88 = 2, 14 - the rest of 16byte "ZX3..." packet
 //				echo '     __________cntd_crc='.$counted_crc;
 				if ($counted_crc==$crc) {
 					$_SESSION['cadi_status']['valve_failed'] = $valve_failed;	// failed valves flags
@@ -174,8 +174,8 @@ switch ($packet_type) {
 				$current_ec = ord($last_packet[2]);		// TxBuffer[4] = currentEc;
 				$current_ph = ord($last_packet[3]);		// TxBuffer[5] = currentPh;
 	
-				$crc = ord($last_packet[13]);
-				$counted_crc = crc_block(2, $last_packet, 13);	// 90 xor 88 = 2, 14 - the rest of 16byte "ZX3..." packet
+				$crc = ord($last_packet[($packet_sizde-3)]);
+				$counted_crc = crc_block(2, $last_packet, ($packet_sizde-3));	// 90 xor 88 = 2, 14 - the rest of 16byte "ZX3..." packet
 				if ($counted_crc==$crc) {
 			//		$_SESSION['cadi_status']['time'] = $cadi_time;	// Cadi UNIXTIME
 
@@ -189,7 +189,7 @@ switch ($packet_type) {
 				for ($i=0; $i<=strlen($last_packet);$i++) {
 						$hexpacket .= sprintf("\\x%02x",ord($last_packet[$i]));
 					}
-				//	echo $hexpacket; 					
+					//echo $hexpacket; 					
 	break;
 }
 
