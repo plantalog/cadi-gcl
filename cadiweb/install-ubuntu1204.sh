49ab1e1e4fad06047f7c33cf3e8c50bdc54a2621
plantalog <github2@aquaplant.ee>
2014-06-04 01:56:48 +0200

    ubuntu 12.04 installation script

--- cadiweb/install-ubuntu1204.sh
+++ cadiweb/install-ubuntu1204.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# this script "installs" cadiweb on Ubuntu 14.04 LTS system
-
+# this script "installs" cadiweb on Ubuntu 12.04 LTS system
 # http://embeddedprogrammer.blogspot.com.es/2012/06/ubuntu-openingusing-serial-ports-as.html
 # http://embeddedprogrammer.blogspot.com.es/2012/06/ubuntu-hacking-hc-06-bluetooth-module.html
 
@@ -15,75 +14,75 @@ echo 'installing LAMP server for Ubuntu'
 sudo apt-get -q -y install lamp-server^
 echo 'installing fswebcam application to take photo shots'
 sudo apt-get -q -y install fswebcam
-echo 'creating directory /var/www/html/cm'
-sudo mkdir /var/www/html/cm
-echo 'creating directory /var/www/html/cm/btds for Bluetooth Daemon data and settings'
-sudo mkdir /var/www/html/cm/btds/
-echo 'changing ownership to www-data for directory /var/www/html'
-chown www-data:www-data /var/www/html
-echo 'changing ownership to www-data for directory /var/www/html/cm'
-chown www-data:www-data /var/www/html/cm
+echo 'creating directory /var/www/cm'
+sudo mkdir /var/www/cm
+echo 'creating directory /var/www/cm/btds for Bluetooth Daemon data and settings'
+sudo mkdir /var/www/cm/btds/
+echo 'changing ownership to www-data for directory /var/www'
+chown www-data:www-data /var/www
+echo 'changing ownership to www-data for directory /var/www/cm'
+chown www-data:www-data /var/www/cm
 
-echo 'changing permissions for directory /var/www/html/cm/btds'
-sudo chmod 777 /var/www/html/cm/btds
+echo 'changing permissions for directory /var/www/cm/btds'
+sudo chmod 777 /var/www/cm/btds
 
-echo 'changing ownership for directory /var/www/html/cm/btds'
-sudo chown www-data:www-data /var/www/html/cm/btds
+echo 'changing ownership for directory /var/www/cm/btds'
+sudo chown www-data:www-data /var/www/cm/btds
 
-echo 'creating/flushing /var/www/html/cm/daemon_cmd file (Cadi BT Daemon command log)'
-sudo echo > /var/www/html/cm/daemon_cmd
+echo 'creating/flushing /var/www/cm/daemon_cmd file (Cadi BT Daemon command log)'
+sudo echo > /var/www/cm/daemon_cmd
 
 echo 'changing permissions for file daemon_cmd'
-sudo chmod 777 /var/www/html/cm/daemon_cmd
+sudo chmod 777 /var/www/cm/daemon_cmd
 
-echo 'changing ownership to www-data for /var/www/html/cm/daemon_cmd'
-sudo chown www-data:www-data /var/www/html/cm/daemon_cmd
+echo 'changing ownership to www-data for /var/www/cm/daemon_cmd'
+sudo chown www-data:www-data /var/www/cm/daemon_cmd
 
-echo 'creating/flushing /var/www/html/cm/btds/btd_output file (Cadi BTD output log)'
-sudo echo > /var/www/html/cm/btds/btd_output
+echo 'creating/flushing /var/www/cm/btds/btd_output file (Cadi BTD output log)'
+sudo echo > /var/www/cm/btds/btd_output
 
-echo 'changing permissions for /var/www/html/cm/btds/btd_output'
-sudo chmod 777 /var/www/html/cm/btds/btd_output
+echo 'changing permissions for /var/www/cm/btds/btd_output'
+sudo chmod 777 /var/www/cm/btds/btd_output
 
-echo 'creating/flushing Cadi status CSV file (/var/www/html/cm/cadi_status.csv)'
-sudo echo > /var/www/html/cm/cadi_status.csv
+echo 'creating/flushing Cadi status CSV file (/var/www/cm/cadi_status.csv)'
+sudo echo > /var/www/cm/cadi_status.csv
 
 echo 'changing Cadi status CSV file ownership'
-sudo chown www-data:www-data /var/www/html/cm/cadi_status.csv
+sudo chown www-data:www-data /var/www/cm/cadi_status.csv
 
 echo 'changing Cadi status CSV file permissions'
-sudo chmod 755 /var/www/html/cm/cadi_status.csv
+sudo chmod 755 /var/www/cm/cadi_status.csv
 
-echo 'creating/flushing Cadi serial data dump log file (/var/www/html/cm/serialresp.out)'
-sudo echo > /var/www/html/cm/serialresp.out
+echo 'creating/flushing Cadi serial data dump log file (/var/www/cm/serialresp.out)'
+sudo echo > /var/www/cm/serialresp.out
 
 echo 'changing ownership for Cadi serial data dump log file'
-sudo chown www-data:www-data /var/www/html/cm/serialresp.out
+sudo chown www-data:www-data /var/www/cm/serialresp.out
 
 echo 'changing permissions for Cadi serial data dump log file'
-sudo chmod 755 /var/www/html/cm/serialresp.out
+sudo chmod 755 /var/www/cm/serialresp.out
 
 echo 'changing permissions for Cadiweb parent directory (/var/www/html)'
-sudo chmod 777 /var/www/html
+sudo chmod 777 /var/www
 
 echo 'changing ownership for Cadi parent directory'
-sudo chown www-data:www-data /var/www/html
+sudo chown www-data:www-data /var/www
 
-echo 'changing permissions for all files inside Cadiweb modules files (/var/www/html/cm)'
-cd /var/www/html/cm
+echo 'changing permissions for all files inside Cadiweb modules files (/var/www/cm)'
+cd /var/www/cm
 sudo chmod 755 *
 
 echo 'removing default Apache index.html page'
-sudo rm -rf /var/www/html/index.html
+sudo rm -rf /var/www/index.html
 
 
 # add BT daemon startup line into /etc/rc.local
-sudo grep -Fxq "/bin/sh /var/www/html/cm/btds/btd_start.sh" /etc/rc.local
+sudo grep -Fxq "/bin/sh /var/www/cm/btds/btd_start.sh" /etc/rc.local
 RESPONSE=$?
 if [ $RESPONSE -eq 1 ];then
-	sudo sed -i -e '$i/bin/sh /var/www/html/cm/btds/btd_start.sh' /etc/rc.local ;
+	sudo sed -i -e '$i/bin/sh /var/www/cm/btds/btd_start.sh' /etc/rc.local ;
 	echo 'startup string added to /etc/rc.local';
-elif [ $RESPONSE -eq 0 ];then
+else
 	echo 'startup string already exists in /etc/rc.local' ;
 fi
 
@@ -100,19 +99,18 @@ sudo echo "cadiweb/" >> .git/info/sparse-checkout
 sudo git pull origin master
 
 cd /tmp/cadi-gcl/cadiweb/
-sudo cp -rf * /var/www/html/
+sudo cp -rf * /var/www/
 
-echo 'changing permissions for Cadi BTDaemon startup script (/var/www/html/cm/btds/btd_start.sh)'
-sudo chmod 777 /var/www/html/cm/btds/btd_start.sh
-# sudo chown www-data:www-data /var/www/html/cm/btds/btd_start.sh
+echo 'changing permissions for Cadi BTDaemon startup script (/var/www/cm/btds/btd_start.sh)'
+sudo chmod 777 /var/www/cm/btds/btd_start.sh
+# sudo chown www-data:www-data /var/www/cm/btds/btd_start.sh
 # sudo l2ping 20:13:06:14:34:06
 # http://wiki.openmoko.org/wiki/Manually_using_Bluetooth
 # http://bluelimn.tistory.com/m/post/715
 
-echo 'changing permissions for Cadi BTDaemon (/var/www/html/cm/bt_daemon.php)'
-sudo chmod 777 /var/www/html/cm/bt_daemon.php
+echo 'changing permissions for Cadi BTDaemon (/var/www/cm/bt_daemon.php)'
+sudo chmod 777 /var/www/cm/bt_daemon.php
 
-cd /var/lib/bluetooth/
 # http://scottada.ms/article/adding-so-called-virtual-bluetooth-serial-port-ubuntu-1110-using-atheros-ar3011-based-usb
 sdptool add --channel=14 SP
 cd /var/lib/bluetooth/
